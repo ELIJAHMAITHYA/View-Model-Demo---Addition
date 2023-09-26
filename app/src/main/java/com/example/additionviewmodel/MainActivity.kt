43065daf6d6763f0d7ecf6356636434db4ea4d84
emmanuel-mutua <emmanuel-mutua>
2023-09-26 16:17:40 +0300

    add sum working

--- app/src/main/java/com/example/additionviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/additionviewmodel/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.additionviewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.additionviewmodel.ui.theme.AdditionViewModelTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +20,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AdditionViewModelTheme {
+                val viewModel : GameViewModel = viewModel()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-AppScreen(viewModel = GameViewModel())
+                    AppScreen(viewModel = viewModel)
                 }
             }
         }
